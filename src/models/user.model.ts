687ce5893b6a9fe758612733f400288107267170
Ifeanyi Mbata <mac@macs-MacBook-Pro-2.local>
2026-04-19 05:53:29 +0100

    fix: resolve TS error in user model and update gitignore

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -58,13 +58,12 @@ const userSchema = new mongoose.Schema<IUser>(
 );
 
 // Encrypt password using bcrypt
-userSchema.pre<IUser>('save', async function (next) {
+userSchema.pre<IUser>('save', async function () {
     if (!this.isModified('password')) {
-        return next();
+        return;
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password as string, salt);
-    next();
 });
 
 // Match user entered password to hashed password in database
